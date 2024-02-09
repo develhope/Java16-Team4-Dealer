f0953a78ecfe7f9439118c36bd7ab6a296dcd9f1
LucaSavini <luca.savini.96@gmail.com>
2024-02-09 11:31:45 +0100

    migliorata classe noleggio

--- src/main/java/com/develhope/spring/transazioni/noleggio/entity/Noleggio.java
+++ src/main/java/com/develhope/spring/transazioni/noleggio/entity/Noleggio.java
@@ -1,5 +1,6 @@
 package com.develhope.spring.transazioni.noleggio.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -13,14 +14,29 @@ import java.time.OffsetDateTime;
 @Setter
 @Getter
 public class Noleggio {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String dataInizio;
-    private String dataFine;
+
+    @Column(name = "dataInizio", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime dataInizio;
+
+    @Column(name = "dataFine", nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")
+    private OffsetDateTime dataFine;
+
+    @Column(name = "costoGiornaliero", nullable = false)
     private BigDecimal costoGiornaliero;
+
+    @Column(name = "costoTotale", nullable = false)
     private BigDecimal costoTotale;
+
+    @Column(name = "pagato", nullable = false)
     private boolean pagato;
+
+    @Column(name = "noleggiato", nullable = false)
     private boolean noleggiato;
 
 }
--- src/main/java/com/develhope/spring/transazioni/noleggio/service/NoleggioService.java
+++ src/main/java/com/develhope/spring/transazioni/noleggio/service/NoleggioService.java
@@ -2,7 +2,6 @@ package com.develhope.spring.transazioni.noleggio.service;
 
 import com.develhope.spring.transazioni.noleggio.entity.Noleggio;
 import com.develhope.spring.transazioni.noleggio.repository.NoleggioRepo;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
