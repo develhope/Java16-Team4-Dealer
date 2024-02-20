8b4ebc1ed56fbc619078b306944058537f6144c3
Amonizz <andrea.monizza@gmail.com>
2024-02-20 21:16:08 +0100

    added controller Veicolo with endpoints (testing needed)

---
+++ src/main/java/com/develhope/spring/veichles/controllers/VeicoloController.java
@@ -0,0 +1,163 @@
+package com.develhope.spring.veichles.controllers;
+
+
+import com.develhope.spring.veichles.dto.VeicoloRequest;
+import com.develhope.spring.veichles.dto.VeicoloResponse;
+import com.develhope.spring.veichles.entity.StatoVendita;
+import com.develhope.spring.veichles.service.VeicoloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@RestController
+public class VeicoloController {
+
+    @Autowired
+    private VeicoloService veicoloService;
+
+
+    @GetMapping("/veicolo/{id}")
+    public VeicoloResponse getVeicolo(@PathVariable Long id) {
+        return this.veicoloService.findById(id);
+        //return new VeicoloResponse(); per prendere il JSON pulito
+    }
+
+    @GetMapping("/veicoli")
+    public List<VeicoloResponse> getAll() {
+        return this.veicoloService.readAll();
+    }
+
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByMarca(@PathVariable (name = "marca") String marca){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.readAllByMarca(marca);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByModello(@PathVariable (name = "modello") String modello){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.readAllByModello(modello);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByPrezzo(@PathVariable (name = "prezzo") BigDecimal prezzo){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.readAllByPrezzo(prezzo);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByUsato(@PathVariable (name = "usato") boolean usato){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.readAllByUsato(usato);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByStatoVendita(@PathVariable (name = "statoVendita") StatoVendita statoVendita){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.findAllByStatoVendita(statoVendita);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+    @GetMapping("/findveicoli/perstatovendita")
+    public ResponseEntity<List<VeicoloResponse>> findVeicoloByStatoVenditaAsc(){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.findAllByStatoVenditaAsc();
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+    @GetMapping("/findveicoli")
+    public ResponseEntity<List<VeicoloResponse>> findAllByRange(
+            @PathVariable (name = "min") BigDecimal min,
+            @PathVariable (name = "max") BigDecimal max){
+        try {
+            List<VeicoloResponse> response = this.veicoloService.findAllByRange(min, max);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PatchMapping("/usato")
+    public ResponseEntity<VeicoloResponse> findAllByRange(
+            @PathVariable (name = "isUsato") Boolean isUsato,
+            @PathVariable (name = "idVeicolo") Long idVeicolo){
+        try {
+            VeicoloResponse response = this.veicoloService.patchStatoUsato(idVeicolo,isUsato);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+
+
+
+
+
+    @PostMapping("/addVeicolo")
+    public VeicoloResponse addVeicolo(@RequestBody VeicoloRequest veicoloRequest) {
+        return veicoloService.createVeicolo(veicoloRequest);
+
+    }
+
+    @PutMapping("/updateveicolo/{id}")
+    public ResponseEntity<VeicoloResponse> updateVeicolo(@RequestBody VeicoloRequest veicoloRequest, @PathVariable Long id) {
+        VeicoloResponse veicoloAggiornato = veicoloService.updateVeicolo(veicoloRequest, id);
+        if (veicoloAggiornato != null) {
+            return ResponseEntity.ok(veicoloAggiornato);
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @PatchMapping("/modificaveicolo/{id}")
+    public ResponseEntity<VeicoloResponse> patchVeicolo(@RequestBody VeicoloRequest veicoloRequest, @PathVariable Long id) {
+        try {
+            VeicoloResponse response = this.veicoloService.patchByVeicoloRequest(id, veicoloRequest);
+            return ResponseEntity.ok(response);
+        }catch (Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+    }
+
+    @DeleteMapping("/deleteVeicolo/{id}")
+    public ResponseEntity<String> deleteVeicolo(@PathVariable Long id) {
+        try {
+
+            return veicoloService.deleteVeicoloById(id)
+                    ? ResponseEntity.ok("Veicolo rimosso con successo")
+                    : ResponseEntity.status(HttpStatus.NOT_FOUND).body("Veicolo non trovato");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Errore durante l'eliminazione del veicolo");
+        }
+    }
+
+
+}
