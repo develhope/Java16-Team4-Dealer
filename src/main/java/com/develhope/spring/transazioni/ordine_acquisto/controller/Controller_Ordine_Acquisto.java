b9ac33fed2c73163fcd95c86bdfdbb0f3582f7c9
Amonizz <andrea.monizza@gmail.com>
2024-02-21 17:41:57 +0100

    avanzamento su controller ordine

--- src/main/java/com/develhope/spring/transazioni/ordine_acquisto/controller/Controller_Ordine_Acquisto.java
+++ src/main/java/com/develhope/spring/transazioni/ordine_acquisto/controller/Controller_Ordine_Acquisto.java
@@ -1,10 +1,46 @@
 package com.develhope.spring.transazioni.ordine_acquisto.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.develhope.spring.transazioni.ordine_acquisto.dto.OrdineAcquistoRequest;
+import com.develhope.spring.transazioni.ordine_acquisto.dto.OrdineAcquistoResponse;
+import com.develhope.spring.transazioni.ordine_acquisto.entity.StatoOrdine;
+import com.develhope.spring.transazioni.ordine_acquisto.service.OrdineAcquistoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Acquisto")
 
 public class Controller_Ordine_Acquisto {
+
+    @Autowired
+    private OrdineAcquistoService ordineAcquistoService;
+
+    @GetMapping("/oa/{id}")
+    public OrdineAcquistoResponse getOAById(@PathVariable Long id){
+        return ordineAcquistoService.findOAById(id);
+    }
+
+    @GetMapping("/oa/all")
+    public List<OrdineAcquistoResponse> getAllOA(){
+        return ordineAcquistoService.findAllOA();
+    }
+
+    @GetMapping("/oa/allbystatus")
+    public List<OrdineAcquistoResponse> getAllOAByStatus(@RequestParam StatoOrdine statoOrdine){
+        return ordineAcquistoService.findAllOAByStatoOrdine(statoOrdine);
+    }
+    @GetMapping("/oa/allstatus")
+    public List<OrdineAcquistoResponse> findAllOAByStatoOrdineAsc(){
+        return ordineAcquistoService.findAllOAByStatoOrdineAsc();
+    }
+
+//    @PostMapping("/create/oa/{idcustomer}/{idveicolo}")
+//    public OrdineAcquistoResponse findAllOAByStatoOrdineAsc(@RequestBody OrdineAcquistoRequest requestA,
+//                                                            Long idCustomer,
+//                                                            Long idVeicolo,
+//                                                            @RequestParam(name = "idvendor",required = false) Long idVendor){
+//
+//    }
 }
