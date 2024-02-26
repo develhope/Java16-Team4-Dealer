c0eeff11d7e46e824c5fc1407fa4c454324da2fa
AndreaLirosi <lirosiandrea@gmail.com>
2024-02-26 19:32:04 +0100

    aggiunto logging feature

---
+++ src/main/java/com/develhope/spring/feature/user/UserServiceV1.java
@@ -0,0 +1,55 @@
+package com.develhope.spring.feature.user;
+
+
+import com.develhope.spring.feature.user.dto.CreateUserRequestV1;
+import com.develhope.spring.feature.user.dto.UserDtoV1;
+import com.develhope.spring.feature.user.dto.UserError;
+import com.develhope.spring.feature.user.entities.UserEntity;
+import io.vavr.control.Either;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class UserServiceV1 {
+
+    @Autowired
+    UserRepository userRepository;
+
+    UserDtoV1 createUser(CreateUserRequestV1 userCreationRequest) {
+        UserEntity userToSave = new UserEntity(userCreationRequest.getEmail(), userCreationRequest.getName(), userCreationRequest.getAge());
+        UserEntity savedUserEntity = userRepository.save(userToSave);
+        return savedUserEntity.toDto();
+    }
+
+    Either<UserError, Boolean> requestDeleteUser(int userId) {
+        Optional<UserEntity> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            try {
+                userRepository.deleteById(userId);
+                return Either.right(true);
+            } catch (Exception ex) {
+                return Either.left(
+                        new UserError(
+                                419,
+                                "Error while deleting user with id: " + userId + " with error: " + ex.getMessage())
+                );
+            }
+        } else {
+            return Either.left(
+                    new UserError(
+                            404,
+                            "User with id: " + userId + " not found"
+                    )
+            );
+        }
+    }
+
+    List<UserDtoV1> getAllUsers() {
+        List<UserEntity> userEntities = userRepository.findAll();
+        return userEntities.stream().map(UserEntity::toDto).toList();
+    }
+}
---
+++ src/main/java/com/develhope/spring/feature/user/dto/UserDtoV1.java
@@ -0,0 +1,71 @@
+package com.develhope.spring.feature.user.dto;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class UserDtoV1 {
+    private int id;
+    @NotBlank
+    private String email;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private int age;
+
+    public UserDtoV1() {
+    }
+
+    public UserDtoV1(int id, String email, String name, int age) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
+
+    public UserDtoV1(String email, String name, int age) {
+        this.email = email;
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "UserDtoV1{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
