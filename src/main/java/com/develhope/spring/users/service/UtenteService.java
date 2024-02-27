242a9e1e155b2b3afeaff1de493649a1c2f45f9e
LucaSavini <luca.savini.96@gmail.com>
2024-02-27 11:50:49 +0100

    Aggiunte rotte ControllerUtente

--- src/main/java/com/develhope/spring/users/service/UtenteService.java
+++ src/main/java/com/develhope/spring/users/service/UtenteService.java
@@ -19,12 +19,14 @@ import com.develhope.spring.veichles.service.VeicoloService;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UtenteService {
 
     @Autowired
