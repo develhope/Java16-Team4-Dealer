cd6880aa7f0feab68d306123134622e9d3c18379
LucaSavini <luca.savini.96@gmail.com>
2024-02-28 17:44:22 +0100

    Creata documentazione swagger per UtenteController ed implementato il try/catch nelle rotte

--- src/main/java/com/develhope/spring/users/controller/UtenteController.java
+++ src/main/java/com/develhope/spring/users/controller/UtenteController.java
@@ -6,7 +6,10 @@ import com.develhope.spring.users.dto.UpdateUtenteRequest;
 import com.develhope.spring.users.dto.UtenteResponse;
 import com.develhope.spring.users.entity.Utente;
 import com.develhope.spring.users.service.UtenteService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.vavr.control.Either;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,12 @@ public class UtenteController {
     @Autowired
     UtenteService utenteService;
 
+    @Operation(summary = "Crea un nuovo utente", description = "Crea un nuovo utente in base alle informazioni fornite nel corpo della richiesta.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente creato correttamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Richiesta non valida"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error - Errore interno del server")
+    })
     @PostMapping(path = "/add")
     public ResponseEntity<UtenteResponse> createUser(@RequestBody CreateUtenteRequest createUtenteRequest) {
         UtenteResponse savedUser = utenteService.createUtente(createUtenteRequest);
@@ -29,33 +38,87 @@ public class UtenteController {
     }
 
 
+    @Operation(summary = "Ottieni tutti gli utenti", description = "Recupera tutti gli utenti nel sistema.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Elenco di tutti gli utenti"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Richiesta non valida"),
+            @ApiResponse(responseCode = "404", description = "Utenti non trovati"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @GetMapping(path = "/getAll")
     public ResponseEntity<List<Utente>> getAllUsers() {
         return new ResponseEntity<>(utenteService.getAll(), HttpStatus.OK);
     }
 
+
+    @Operation(summary = "Ottieni utente per ID", description = "Recupera un utente specifico in base al suo ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente recuperato correttamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Richiesta non valida"),
+            @ApiResponse(responseCode = "404", description = "Controller non trovato"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error"),
+            @ApiResponse(responseCode = "512", description = "Utente non trovato")
+    })
     @GetMapping(path = "/user/{id}")
     public ResponseEntity<Utente> getById(@PathVariable Long id) {
-        return new ResponseEntity<>(utenteService.getById(id), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(utenteService.getById(id), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity("Utente non trovato", HttpStatus.valueOf(512));
+        }
     }
 
 
+    @Operation(summary = "Elimina un utente", description = "Elimina un utente specifico in base al suo ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente eliminato correttamente"),
+            @ApiResponse(responseCode = "404", description = "Utente non trovato"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @DeleteMapping(path = "/delete/{userId}")
     public ResponseEntity<String> deletAUser(@PathVariable Long id) {
-       utenteService.deleteUtente(id);
-       return ResponseEntity.ok("Utente cancellato con successo");
+        utenteService.deleteUtente(id);
+        try {
+            return ResponseEntity.ok("Utente cancellato con successo");
+        } catch (Exception e) {
+            return new ResponseEntity("Utente non trovato", HttpStatus.valueOf(512));
+        }
     }
 
+
+    @Operation(summary = "Aggiorna l'intero utente", description = "Aggiorna l'intero utente in base al corpo della richiesta.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente aggiornato correttamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Richiesta non valida"),
+            @ApiResponse(responseCode = "404", description = "Utente non trovato"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @PutMapping(path = "/Fullupdate/{id}")
-    public ResponseEntity<String> fullUpdateUtente(@RequestBody UpdateUtenteRequest request, Long id) {
+    public ResponseEntity<String> fullUpdateUtente(@PathVariable Long id, @RequestBody UpdateUtenteRequest request) {
         utenteService.updateUtente(id, request);
-        return ResponseEntity.ok("Utente aggionato con successo");
+        try {
+            return ResponseEntity.ok("Utente aggionato con successo");
+        } catch (Exception e) {
+            return new ResponseEntity("Utente non trovato", HttpStatus.valueOf(512));
+        }
     }
 
+
+    @Operation(summary = "Aggiorna informazioni parziali dell'utente", description = "Aggiorna informazioni specifiche dell'utente in base al corpo della richiesta.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Utente modificato correttamente"),
+            @ApiResponse(responseCode = "400", description = "Bad Request - Richiesta non valida"),
+            @ApiResponse(responseCode = "404", description = "Utente non trovato"),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error")
+    })
     @PatchMapping(path = "/update/{id}")
-    public ResponseEntity<String> updateUtente(@RequestBody UpdateUtenteRequest request, Long id) {
+    public ResponseEntity<String> updateUtente(@PathVariable Long id, @RequestBody UpdateUtenteRequest request) {
         utenteService.patchUtente(request, id);
-        return ResponseEntity.ok("Utente modificato con successo");
+        try {
+            return ResponseEntity.ok("Utente modificato con successo");
+        } catch (Exception e) {
+            return new ResponseEntity("Utente non trovato", HttpStatus.valueOf(512));
+        }
     }
 
 }
